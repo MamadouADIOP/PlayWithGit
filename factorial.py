39c37fa760b8e9324fa88ea13af4162c166bbe85
CORPORATE\mdiop <mdiop@teksystems.com>
2024-11-01 20:40:14 -0400

    Modification done in feature branch Rebase done while being placed in feature branch

--- factorial.py
+++ factorial.py
@@ -1,6 +1,9 @@
 # Python code to calculate the factorial of a number 
 
+
 #Another comment added Done In main branch
+#Another comment added Done in feature branch
+
 def calculate_factorial(number): 
     if number == 0 or number == 1: 
         return 1
